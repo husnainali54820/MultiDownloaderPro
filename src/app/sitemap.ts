4ad6a34edda8becfa1d1b70a3c418006bed4d00b
husnainali54820 <husnainali54820@gmail.com>
2025-11-24 08:39:29 +0500

    Update base URL in sitemap to new domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://multi-downloader.com';
+  const baseUrl = 'https://mdownloader.vercel.app';
 
   const downloaderPages = [
     '/youtube-downloader',
